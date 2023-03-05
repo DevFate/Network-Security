36f47955f41acda69f6c71330ff5ccf5473338fc
ryan-awad <ryanawad@yahoo.com>
2023-03-05 02:31:31 -0500

    IM A GENIUS

--- camera/server2.py
+++ camera/server2.py
@@ -1,20 +1,47 @@
-from mlsocket import MLSocket
+import socket
 import cv2
+import numpy as np
 
+# Define the host and port to listen on
 HOST = 'localhost'
 PORT = 8000
 
-with MLSocket() as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    conn, address = s.accept()
+BUFFER = 76032
 
-    with conn:
-        while True:
-            data = conn.recv(1024) # This will block until it receives all the data send by the client, with the step size of 1024 bytes.
-            cv2.imshow('Input', data)
+# Create a TCP socket and bind it to the host and port
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind((HOST, PORT))
 
-            c = cv2.waitKey(1)
-            if c == 27: # ESC
-                s.close()
-                break
+# Listen for incoming connections
+server_socket.listen()
+
+print(f"Server is listening on {HOST}:{PORT}")
+
+# Wait for a client to connect
+client_socket, client_address = server_socket.accept()
+
+print(f"Client connected from {client_address[0]}:{client_address[1]}")
+
+# Receive data from the client
+while True:
+    data = client_socket.recv(BUFFER)
+    if not data:
+        print("STOPPING")
+        break
+    
+    frame = np.frombuffer(data, dtype=np.uint8)
+    frame = frame.reshape(144, 176, 3)
+
+    cv2.imshow('Input', frame)
+
+    c = cv2.waitKey(1)
+    if c == 27: # ESC
+        client_socket.close()
+        break
+
+
+# Close the client socket
+client_socket.close()
+
+# Close the server socket
+server_socket.close()
