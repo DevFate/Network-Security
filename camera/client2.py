99be340ed7f4c0436d29cf38fbe723ca370ec5ce
ryan-awad <ryanawad@yahoo.com>
2023-03-05 04:09:12 -0500

    please

--- camera/client2.py
+++ camera/client2.py
@@ -1,28 +1,31 @@
 import cv2
 import socket
 import numpy as np
-from mlsocket import MLSocket
 
-HOST = '172.20.10.14'
+HOST = 'localhost'
 PORT = 8000
 
-with MLSocket() as s:
-    s.connect((HOST, PORT))
-    print(f"Connected to server {HOST}:{PORT}")
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect((HOST, PORT))
+print(f"Connected to server {HOST}:{PORT}")
 
-    capture = cv2.VideoCapture(0)
+capture = cv2.VideoCapture(0)
 
-    if not capture.isOpened():
-        raise IOError("Cannot access the webcam!")
+if not capture.isOpened():
+    raise IOError("Cannot access the webcam!")
 
-    while True:
-        ret, frame = capture.read()
-        s.send(frame)
+while True:
+    ret, frame = capture.read()
+    frame = cv2.resize(frame, (100, 100))
+    frame_bytes = frame.tobytes()
+    client_socket.send(frame_bytes)
 
-        c = cv2.waitKey(1)
-        if c == 27: # ESC
-            s.close()
-            break
+    c = cv2.waitKey(1)
+    if c == 27: # ESC
+        client_socket.close()
+        break
 
-    capture.release()
-    cv2.destroyAllWindows()
+
+
+capture.release()
+cv2.destroyAllWindows()
