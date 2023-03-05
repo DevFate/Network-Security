70b173ad2da27d1c13cd71cbe00b11e98fec97c7
ryan-awad <ryanawad@yahoo.com>
2023-03-05 03:24:13 -0500

    test

--- camera/client2.py
+++ camera/client2.py
@@ -1,38 +1,28 @@
 import cv2
 import socket
 import numpy as np
+from mlsocket import MLSocket
 
-HOST = 'localhost'
+HOST = '172.20.10.14'
 PORT = 8000
 
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect((HOST, PORT))
-print(f"Connected to server {HOST}:{PORT}")
+with MLSocket() as s:
+    s.connect((HOST, PORT))
+    print(f"Connected to server {HOST}:{PORT}")
 
-capture = cv2.VideoCapture(0)
+    capture = cv2.VideoCapture(0)
 
-if not capture.isOpened():
-    raise IOError("Cannot access the webcam!")
+    if not capture.isOpened():
+        raise IOError("Cannot access the webcam!")
 
-while True:
-    ret, frame = capture.read()
-    frame = cv2.resize(frame, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_AREA)
-    #print(frame.tobytes().__sizeof__())
-    #print(frame.shape)
-    frame_bytes = frame.tobytes()
-    back_to_matrix = np.frombuffer(frame_bytes, dtype=np.uint8)
-    back_to_matrix.reshape(144, 176, 3)
-    #print(back_to_matrix.shape)
-    #cv2.imshow('Input', back_to_matrix)
-    
-    client_socket.send(frame_bytes)
+    while True:
+        ret, frame = capture.read()
+        s.send(frame)
 
-    c = cv2.waitKey(1)
-    if c == 27: # ESC
-        client_socket.close()
-        break
+        c = cv2.waitKey(1)
+        if c == 27: # ESC
+            s.close()
+            break
 
-
-
-capture.release()
-cv2.destroyAllWindows()
+    capture.release()
+    cv2.destroyAllWindows()
