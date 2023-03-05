b4cebe493757772c33b6d6dff6e109da73cb8239
DevFate <umerqamar0333@gmail.com>
2023-03-04 22:26:36 -0500

    Assynchronous coms

---
+++ Server.py
@@ -0,0 +1,43 @@
+import socket
+import sys
+import threading
+
+# create a TCP/IP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# bind the socket to a specific address and port
+server_address = ('10.196.29.20', 8000)
+sock.bind(server_address)
+
+# listen for incoming connections
+sock.listen(1)
+def send(conn, message):
+    conn.sendall(message.encode())
+
+def receive(conn):
+    while True:
+        data = conn.recv(1024)
+        if data:
+            print("Received: {}".format(data.decode()))
+
+while True:
+    # wait for a connection
+    print('waiting for a connection')
+    connection, client_address = sock.accept()
+
+    try:
+        print('connection from', client_address)
+
+        receive_thread = threading.Thread(target=receive, args=(connection,))
+        receive_thread.start()
+
+        # read user input and send to the client
+        while True:
+            message = input("Enter a message to send: ")
+            if not message:
+                break
+            send(connection, message)
+
+    finally:
+        # clean up the connection
+        connection.close()
